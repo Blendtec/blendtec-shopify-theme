629d442a201fbda942cd67e8dc866477d386bb27
Stone C. Lasley <stonelasley@gmail.com>
2018-06-04 09:47:41 -0700

    chore(home.js): rename html element ids
    BTCOM-25-19

--- src/assets/scripts/templates/home.js
+++ src/assets/scripts/templates/home.js
@@ -1,31 +1,31 @@
-(function() {
+(function () {
 
   var page = {
     cache: {},
-    init: function() {
+    init: function () {
       // INIT PAGE LEVEL COMPONENTS
       return true;
     },
     hero: {
-      init: function() {
+      init: function () {
         page.hero.cacheSelectors();
         page.cache.$playBtn.on('click', page.hero.showPopUp);
         $(window).on('resize', page.hero.resizeVideo);
         $(document).on('bt:intent:dismiss', page.hero.stopVideo);
         page.hero.resizeVideo();
       },
-      cacheSelectors: function() {
+      cacheSelectors: function () {
         page.cache.$videoModal = $('#video__modal');
         page.cache.$playBtn = $('.btn-hero--play');
-        page.cache.$videoSpinner = $('#videoLoadingSpinner');
-        page.cache.$videoWrapper = $('#videoWrapper');
-        page.cache.$videoId = $('#videoIdToShow');
+        page.cache.$videoSpinner = $('#video-loading-spinner');
+        page.cache.$videoWrapper = $('#modal-video-wrapper');
+        page.cache.$videoId = $('#modal-video-frame');
       },
-      startVideo: function() {
+      startVideo: function () {
         var $videoEl = page.cache.$videoId;
         var videoSrc = $videoEl.attr('src');
         $videoEl.attr('src', videoSrc += '&autoplay=1');
-        $videoEl.load(function() {
+        $videoEl.load(function () {
           page.cache.$videoSpinner.hide();
           page.cache.$videoWrapper.animate(
             {
@@ -35,18 +35,18 @@
           );
         });
       },
-      stopVideo: function() {
+      stopVideo: function () {
         var $videoEl = page.cache.$videoId;
         var videoSrc = $videoEl.attr('src');
         $videoEl.attr('src', videoSrc.replace('&autoplay=1', ''));
         page.cache.$videoWrapper.css('opacity', 0);
         page.cache.$videoSpinner.show();
       },
-      showPopUp: function() {
+      showPopUp: function () {
         page.cache.$videoModal.addClass('show');
         page.hero.startVideo();
       },
-      resizeVideo: function() {
+      resizeVideo: function () {
         var widthToSet = $(window).width() - 100;
         var maxWidth = 1000;
 
