36a544158c46c2ff63a5c589dafe5b5a6475b76e
Stone C. Lasley <stonelasley@gmail.com>
2018-06-04 10:47:55 -0700

    fix(bt-hero-section): fix json scrambled by autoformat

--- src/assets/scripts/templates/home.js
+++ src/assets/scripts/templates/home.js
@@ -1,33 +1,33 @@
 (function () {
 
   var page = {
-    cache: {},
+    el: {},
     init: function () {
       // INIT PAGE LEVEL COMPONENTS
       return true;
     },
     hero: {
       init: function () {
-        page.hero.cacheSelectors();
-        page.cache.$playBtn.on('click', page.hero.showPopUp);
+        page.hero.elSelectors();
+        page.el.$playBtn.on('click', page.hero.showPopUp);
         $(window).on('resize', page.hero.resizeVideo);
         $(document).on('bt:intent:dismiss', page.hero.stopVideo);
         page.hero.resizeVideo();
       },
-      cacheSelectors: function () {
-        page.cache.$videoModal = $('#video__modal');
-        page.cache.$playBtn = $('.btn-hero--play');
-        page.cache.$videoSpinner = $('#video-loading-spinner');
-        page.cache.$videoWrapper = $('#modal-video-wrapper');
-        page.cache.$videoId = $('#modal-video-frame');
+      elSelectors: function () {
+        page.el.$videoModal = $('#video__modal');
+        page.el.$playBtn = $('.btn-hero--play');
+        page.el.$videoSpinner = $('#video-loading-spinner');
+        page.el.$videoWrapper = $('#modal-video-wrapper');
+        page.el.$videoId = $('#modal-video-frame');
       },
       startVideo: function () {
-        var $videoEl = page.cache.$videoId;
+        var $videoEl = page.el.$videoId;
         var videoSrc = $videoEl.attr('src');
         $videoEl.attr('src', videoSrc += '&autoplay=1');
         $videoEl.load(function () {
-          page.cache.$videoSpinner.hide();
-          page.cache.$videoWrapper.animate(
+          page.el.$videoSpinner.hide();
+          page.el.$videoWrapper.animate(
             {
               opacity: 1,
             },
@@ -36,17 +36,18 @@
         });
       },
       stopVideo: function () {
-        var $videoEl = page.cache.$videoId;
+        var $videoEl = page.el.$videoId;
         var videoSrc = $videoEl.attr('src');
         $videoEl.attr('src', videoSrc.replace('&autoplay=1', ''));
-        page.cache.$videoWrapper.css('opacity', 0);
-        page.cache.$videoSpinner.show();
+        page.el.$videoWrapper.css('opacity', 0);
+        page.el.$videoSpinner.show();
       },
       showPopUp: function () {
-        page.cache.$videoModal.addClass('show');
+        page.el.$videoModal.addClass('show');
         page.hero.startVideo();
       },
       resizeVideo: function () {
+        var $videoWrapper = page.el.$videoWrapper;
         var widthToSet = $(window).width() - 100;
         var maxWidth = 1000;
 
@@ -56,8 +57,8 @@
 
         var heightToSet = widthToSet * 0.5625;
 
-        page.cache.$videoWrapper.width(widthToSet);
-        page.cache.$videoWrapper.height(heightToSet);
+        $videoWrapper.width(widthToSet);
+        $videoWrapper.height(heightToSet);
 
       },
     },
