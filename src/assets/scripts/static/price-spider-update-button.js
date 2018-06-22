feabeaba725ae8f012008c3f4d5f48a6f7434fa4
Reid Kuttler <reidkuttler@gmail.com>
2018-06-22 13:21:24 -0700

    code corrections and style updates

--- src/assets/scripts/static/price-spider-update-button.js
+++ src/assets/scripts/static/price-spider-update-button.js
@@ -1,9 +1,12 @@
+(function() {
 	function changePriceSpiderButton () {
 		var currentVariant = new URL(window.location.href).searchParams.get('variant');
 		var variantSKU = $('.ps-widget').attr('ps-sku-'+currentVariant);
 		$('.ps-widget').attr('ps-sku', variantSKU);
 		PriceSpider.rebind();
 	}
+
+var detectPriceSpiderChange = function () {
 	$(document).on('change', '#productSelect-option-0', function() {
 		changePriceSpiderButton();
 	});
@@ -11,3 +14,16 @@
 	$(document).on('change', '#productSelect-option-1', function() {
 		changePriceSpiderButton();
 	});
+
+};
+
+var deferJQuery = function (func) {
+  if (window.jQuery) {
+  	func();
+  } else {
+    setTimeout(function() { func }, 50);
+  }
+};
+
+deferJQuery(detectPriceSpiderChange);
+})();
